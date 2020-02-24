baf33d1e1f93e8ec8e4e88e4bc41b8985b12289e
kivimango <balazsviktor23@gmail.com>
2020-02-24 18:44:11 +0100

    Added PIC, basic keyboard, timer interrupt handlers

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,12 +1,45 @@
 use crate::println;
+use crate::print;
 use crate::gdt;
 use lazy_static::lazy_static;
+use pc_keyboard::{Keyboard, ScancodeSet1, layouts};
+use pic8259_simple::ChainedPics;
+use spin::Mutex;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+const PIC_1_OFFSET: u8 = 32;
+const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+static PICS: Mutex<ChainedPics> = Mutex::new(
+    // wrong offsets leads to Undefined Behavior
+    unsafe{ ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) }
+);
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+enum InterruptIndex {
+    // Intel 8253 timer uses line 0 of the primary PIC, but we remapped it, so it arrives to the CPU as interrupt 0 + 32 = 32
+    Timer = PIC_1_OFFSET,
+    Keyboard
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
 
 lazy_static! {
     // we loaded a valid TSS and interrupt stack table, we can set the stack index for our double fault handler in the IDT
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
 
         // unsafe because the the caller must ensure that the used index is valid and not already used for another exception.
@@ -14,17 +47,47 @@ lazy_static! {
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-       
         idt
     };
 }
 
+lazy_static! {
+    static ref KEYBOARD : Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1));
+}
+
 pub fn init_idt() {
     IDT.load();
+    unsafe { PICS.lock().initialize(); }
+    x86_64::instructions::interrupts::enable();
+}
+
+extern "x86-interrupt" fn timer_handler(_stack_frame: &mut InterruptStackFrame) {
+    print!(".");
+    eoi(InterruptIndex::Timer.as_u8());
+}
+
+extern "x86-interrupt" fn keyboard_handler(_stack_frame: &mut InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+    use pc_keyboard::{DecodedKey};
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    eoi(InterruptIndex::Keyboard.as_u8());
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
-    println!("Breakpoint exception occured: {:#?}", stack_frame);
+    println!("Breakpoint exception occurred: {:#?}", stack_frame);
 }
 
 /**
@@ -56,5 +119,11 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFra
  * This function is diverging because the x86_64 architecture does not permit returning from a double fault exception.
  */
 extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> !{
-    panic!("Double Fault occured: \n{:#?},\n stopping kernel...", stack_frame);
+    panic!("Double Fault occurred: \n{:#?},\n, error code: {:#?} stopping kernel...", _error_code, stack_frame);
+}
+
+fn eoi(index : u8) {
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(index);
+    }
 }
